dfc095be85f6a2a9bec75170dbfee346db2d6522
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-08-10 20:16:29 -0400

    Update chip styling

--- src/components/Visualizer/StatusBar.js
+++ src/components/Visualizer/StatusBar.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import Box from '@material-ui/core/Box';
 import Chip from "@material-ui/core/Chip";
 import Typography from "@material-ui/core/Typography";
-import CheckCircleOutlinedIcon from '@material-ui/icons/CheckCircleOutlined';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import ClearOutlinedIcon from '@material-ui/icons/ClearOutlined';
 
 export default class StatusBar extends Component {
     render() {
         var status = <Typography variant="subtitle1">Status: {this.props.isSorted ? "Sorted" : "Not sorted"}</Typography>;
-        var icon = this.props.isSorted ? <CheckCircleOutlinedIcon /> : <ClearOutlinedIcon/>;
+        var icon = this.props.isSorted ? <CheckCircleOutlineIcon style={{color: 'white'}}/> : <ClearOutlinedIcon/>;
+        var style = this.props.isSorted? {backgroundColor:'cadetblue', color:'white'} : null;
         return (
             <Box p={0} mx="auto">
-                <Chip icon={icon} label={status}></Chip>
+                <Chip icon={icon} style={style} label={status}></Chip>
             </Box>
         )
     }
