193c3c3c75998a2329430c5f65a1ae05ba74678d
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-08-23 11:36:29 -0400

    Clean up Canvas

--- src/components/Visualizer/Array/Canvas.js
+++ src/components/Visualizer/Array/Canvas.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import { Paper, Box } from "@material-ui/core";
-import ElementBar from "./ElementBar";
+import React, { Component } from 'react';
+import { Paper, Box } from '@material-ui/core';
+import ElementBar from './ElementBar';
 
 export default class Canvas extends Component {
   render() {
     return (
-      <Box p={5} mx='auto' width={1/3}>
-        <Paper elevation={0} >
+      <Box p={5} mx="auto" width={1 / 3}>
+        <Paper elevation={0}>
           {this.props.array.map((number, index) => (
-            <ElementBar key={number + "_" + index} height={number}/>
+            <ElementBar key={`${number}_${index}`} height={number} />
           ))}
         </Paper>
       </Box>
--- src/components/Visualizer/Options/StatusBar.js
+++ src/components/Visualizer/Options/StatusBar.js
@@ -4,14 +4,19 @@ import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import ClearOutlinedIcon from '@material-ui/icons/ClearOutlined';
 
 export default class StatusBar extends Component {
-    render() {
-        var status = <Typography variant="subtitle1">Status: {this.props.isSorted ? "Sorted" : "Not sorted"}</Typography>;
-        var icon = this.props.isSorted ? <CheckCircleOutlineIcon style={{color: 'white'}}/> : <ClearOutlinedIcon/>;
-        var style = this.props.isSorted? {backgroundColor:'cadetblue', color:'white'} : null;
-        return (
-            <Box p={0} mx="auto">
-                <Chip icon={icon} style={style} label={status}></Chip>
-            </Box>
-        )
-    }
+  render() {
+    const status = (
+      <Typography variant="subtitle1">
+        Status:
+        {this.props.isSorted ? 'Sorted' : 'Not sorted'}
+      </Typography>
+    );
+    const icon = this.props.isSorted ? <CheckCircleOutlineIcon style={{ color: 'white' }} /> : <ClearOutlinedIcon />;
+    const style = this.props.isSorted ? { backgroundColor: 'cadetblue', color: 'white' } : null;
+    return (
+      <Box p={0} mx="auto">
+        <Chip icon={icon} style={style} label={status} />
+      </Box>
+    );
+  }
 }
